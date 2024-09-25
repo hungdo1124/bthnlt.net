54c6b994c1306331a18fc28f2b2a88672b19429b
hungdo1124 <hungdo1124@gmail.com>
2024-09-25 08:24:44 +0700

    Add files via upload

---
+++ bai 2 ngay 25.9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+class Program
+{
+    static void Main()
+    {
+        Hashtable people = new Hashtable();
+        Console.Write("Nhap so luong nguoi: ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Nhap ten nguoi thu " + (i + 1) + ": ");
+            string name = Console.ReadLine();
+            Console.Write("Nhap tuoi cua " + name + ": ");
+            int age = int.Parse(Console.ReadLine());
+            people.Add(name, age);
+        }
+        Console.WriteLine("Danh sach ten va tuoi da nhap:");
+        foreach (DictionaryEntry person in people)
+        {
+            Console.WriteLine("Ten: " + person.Key + ", Tuoi: " + person.Value);
+        }
+    }
+}
---
+++ bai3 ngay25.9.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> students = new Dictionary<string, int>();
+        Console.Write("Nhap so luong hoc sinh: ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Nhap ten hoc sinh thu " + (i + 1) + ": ");
+            string name = Console.ReadLine();
+            Console.Write("Nhap diem cua " + name + ": ");
+            int score = int.Parse(Console.ReadLine());
+            students.Add(name, score);
+        }
+        Console.WriteLine("\nDanh sach hoc sinh và diem:");
+        foreach (KeyValuePair<string, int> student in students)
+        {
+            Console.WriteLine("Ten: " + student.Key + ", Diem: " + student.Value);
+        }
+    }
+}
